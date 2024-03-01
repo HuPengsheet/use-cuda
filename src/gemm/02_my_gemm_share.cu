ca29f250a9ea010b066e72c7fbf46366ef7cf4fe
HuPengsheet <1554414201@qq.com>
2024-03-01 17:31:48 +0800

    gemm 02

--- src/gemm/02_my_gemm_share.cu
+++ src/gemm/02_my_gemm_share.cu
@@ -3,10 +3,6 @@
 #include<cuda.h>
 #include<cuda_runtime.h>
 
-
-
-
-
 void compare_array(float * x,float *y,int n)
 {
     int i=0;
@@ -20,12 +16,10 @@ void compare_array(float * x,float *y,int n)
          }
     }
 
-    //if(i==n)  printf("x = y \n");
+    // if(i==n)  printf("x = y \n");
 }
 
-//input a m*k
-//input b k*n
-//output c m*n
+
 void sgemm(size_t m,size_t n,size_t k,float* a,float* b,float* c,float* bias)
 {
     for(int i=0;i<m;i++)
@@ -46,19 +40,6 @@ void sgemm(size_t m,size_t n,size_t k,float* a,float* b,float* c,float* bias)
 
 
 
-// template <int BLOCK>
-// __global__ void cuda_sgemm_forward(size_t m,size_t n,size_t k,float* a,float* b,float* c,float* bias)
-// {
-//     int _m = blockIdx.x * BLOCK + threadIdx.x;
-//     int _n = blockIdx.y * BLOCK + threadIdx.y;
-//     if (_m < m and _n < n) {
-//       float sum = 0.f;
-//       for (int i = 0; i < k; ++i) {
-//         sum += a[_m * k + i] * b[i * n + _n];
-//       }
-//       c[_m * n + _n] = sum+bias[_m];
-//     }
-// }
 
 
 template <int BLOCK>
@@ -68,72 +49,70 @@ __global__ void cuda_sgemm_forward(size_t m,size_t n,size_t k,float* a,float* b,
     const int ty = threadIdx.y;
     const int bx = blockIdx.x;
     const int by = blockIdx.y;
-
     int global_idx= bx*BLOCK+tx;
     int global_idy= by*BLOCK+ty;
 
-    float *begin_a = a + by * BLOCK * k;
-    float *begin_b = b + bx * BLOCK;
+    float *begin_a = a + bx * BLOCK * k;
     float *end_a = begin_a + k;
-    
-    int num=0;
+    float *a_bottom = a+(m-1)*k;
+    float *a_block_bottom = begin_a+(BLOCK-1)*k;
+    int a_x_gap = (a_bottom>=a_block_bottom) ? BLOCK : (BLOCK-(a_block_bottom-a_bottom)/k);
+
+
+    float *begin_b = b + by * BLOCK;
+    float *end_b = b+(k-1)*n;
+    float *b_right = b+n;
+    float *b_block_right = begin_b+BLOCK;
+    int b_y_gap = (b_right>=b_block_right) ? BLOCK : (BLOCK-(b_block_right-b_right));
+        
+
     float sum = 0.f;
-    for (float *a_ptr = begin_a, *b_ptr = begin_b; a_ptr < end_a;
-         a_ptr += BLOCK, b_ptr += BLOCK * n) {
-            num++;
-      __shared__ float ashare[BLOCK][BLOCK];
-      __shared__ float bshare[BLOCK][BLOCK];
-      //printf("global_idy=%d ,global_idx=%d  \n",global_idy,global_idx);
-    //   printf("num= %d \n",num);
-      if(num==2)
-      {
-        if(global_idy<m&&global_idx<n){
-            ashare[ty][tx] = a_ptr[ty * k + tx];
-            bshare[ty][tx] = b_ptr[ty * n + tx];
-            printf("global_idy=%d ,global_idx=%d  \n",global_idy,global_idx);
-        }
-      }
-      __syncthreads();
-
-    //   if(num==1)
-    //   {
-    //     if(global_idx==0&&global_idy==0){
-    //         for(int i=0;i<BLOCK;i++){
-    //             for(int j=0;j<BLOCK;j++)
-    //             {
-    //                 printf("%f ",ashare[i][j]);
-    //             }
-    //             printf("\n");
-    //         }
-    //       }
-    //   }
-
-
-    //   if(global_idx==1&&global_idy==1) printf("**************\n");
-
-    //   if(global_idx==1&&global_idy==1){
-    //     for(int i=0;i<BLOCK;i++){
-    //         for(int j=0;j<BLOCK;j++)
-    //         {
-    //             printf("%f ",bshare[i][j]);
-    //         }
-    //         printf("\n");
-    //     }
-    //   }
-    //   if(global_idx==1&&global_idy==1) printf("**************\n");
-     
-
-  #pragma unroll
-      for (int kk = 0; kk < BLOCK; ++kk) {
-        sum += ashare[ty][kk] * bshare[kk][tx];
-      }
-      __syncthreads();
-    }
-    if(global_idy<m&&global_idx<n)
+    int flag=1;
+    for (float *a_ptr = begin_a, *b_ptr = begin_b; a_ptr < end_a;a_ptr += BLOCK, b_ptr += BLOCK * n) 
     {
-        c[global_idy*m+global_idx] = sum+bias[global_idy];
+
+        __shared__ float ashare[BLOCK][BLOCK];
+        __shared__ float bshare[BLOCK][BLOCK];
+        __shared__ float bias_share[BLOCK];
+
+        float* a_block_right = a_ptr+BLOCK;
+        int a_y_gap = (end_a>=a_block_right) ? BLOCK : (BLOCK-(a_block_right-end_a));
+
+        float* b_block_bottom = b_ptr+(BLOCK-1) * n;
+        int b_x_gap = (end_b>=b_block_bottom) ? BLOCK : (BLOCK-(b_block_bottom-end_b)/n);
+
+
+        if(tx<a_x_gap&&ty<a_y_gap) ashare[tx][ty] = a_ptr[tx * k + ty];
+        if(tx<b_x_gap&&ty<b_y_gap) bshare[tx][ty] = b_ptr[tx * n + ty];
+        if(tx<a_x_gap) bias_share[tx] = bias[(begin_a-a)/k+tx];
+        __syncthreads();
+
+
+
+        #pragma unroll
+        for (int kk = 0; kk < BLOCK; ++kk) 
+        {
+            sum += ashare[tx][kk] * bshare[kk][ty];
+        }
+        if(flag) 
+        {
+            sum+=bias_share[tx];
+            flag=0;
+        }
+        __syncthreads();
+
+        ashare[tx][ty]=0;
+        bshare[tx][ty]=0; 
+        __syncthreads();
+
+
     }
-    
+
+    if(global_idx<m&&global_idy<n)
+    {
+        c[global_idx*n+global_idy] = sum+bias[global_idx];
+    }
+
 }
 
 void cuda_sgemm(size_t m,size_t n,size_t k,float* h_a,float* h_b,float* h_c,float* h_bias)
@@ -175,14 +154,6 @@ void cuda_sgemm(size_t m,size_t n,size_t k,float* h_a,float* h_b,float* h_c,floa
 
     cudaMemcpy(h_c,d_c,c_nbytes,cudaMemcpyDeviceToHost);
 
-    // for(int i=0;i<m;i++){
-    //     for(int j=0;j<n;j++)
-    //     {
-    //         printf("%f ",h_c[i*m+j]);
-    //     }
-    //     printf("\n");
-    // }
-
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
@@ -198,7 +169,7 @@ void test_time()
 
     
 
-    for(int i=17;i<=17;i=i+1)
+    for(int i=2;i<=16384;i=i*2)
     {   
         //printf("start i= %d  \n",i);
         size_t nbytes = i*i*sizeof(float);
@@ -214,8 +185,6 @@ void test_time()
         for(int j=0;j<i*i;j++)
         {
             h_a[j] = dis(gen);
-            //printf("a[%d]=%f ",j,h_a[j]);
-            //printf("\n");
             h_b[j] = dis(gen);  
         }
         
@@ -226,11 +195,11 @@ void test_time()
 
         cuda_sgemm(i,i,i,h_a,h_b,h_c,h_bias);
         
-        if(i<=4096) 
-        {
-            sgemm(i,i,i,h_a,h_b,result,h_bias);
-            compare_array(result,h_c,i*i);
-        }
+        // if(i<=4096) 
+        // {
+        //     sgemm(i,i,i,h_a,h_b,result,h_bias);
+        //     compare_array(result,h_c,i*i);
+        // }
 
         free(h_a);
         free(h_b);
@@ -243,7 +212,7 @@ int main()
 
 
     test_time();
-    //test_time();
+    test_time();
     printf("01_gemm_naive  run  !!!\n");
     return 0;
 }
--- src/gemm/gemm_test.cu
+++ src/gemm/gemm_test.cu
@@ -20,7 +20,7 @@ void compare_array(float * x,float *y,int n)
          }
     }
 
-    //if(i==n)  printf("x = y \n");
+    if(i==n)  printf("x = y \n");
 }
 
 //input a m*k
@@ -46,39 +46,156 @@ void sgemm(size_t m,size_t n,size_t k,float* a,float* b,float* c,float* bias)
 
 
 
+// template <int BLOCK>
+// __global__ void cuda_sgemm_forward(size_t m,size_t n,size_t k,float* a,float* b,float* c,float* bias)
+// {
+//     const int tx = threadIdx.x;
+//     const int ty = threadIdx.y;
+//     const int bx = blockIdx.x;
+//     const int by = blockIdx.y;
+  
+//     float *begin_a = a + by * BLOCK * k;
+//     float *begin_b = b + bx * BLOCK;
+//     float *end_a = begin_a + k;
+  
+//     float sum = 0.f;
+//     for (float *a_ptr = begin_a, *b_ptr = begin_b; a_ptr < end_a;
+//          a_ptr += BLOCK, b_ptr += BLOCK * n) {
+//       __shared__ float ashare[BLOCK][BLOCK];
+//       __shared__ float bshare[BLOCK][BLOCK];
+  
+//       ashare[ty][tx] = a_ptr[ty * k + tx];
+//       bshare[ty][tx] = b_ptr[ty * n + tx];
+//       __syncthreads();
+  
+//   #pragma unroll
+//       for (int kk = 0; kk < BLOCK; ++kk) {
+//         sum += ashare[ty][kk] * bshare[kk][tx];
+//       }
+//       __syncthreads();
+//     }
+  
+//     c[(BLOCK * by + ty) * n + BLOCK * bx + tx] = sum;
+// }
+
 template <int BLOCK>
-__global__ void cuda_sgemm_forward(size_t m,size_t n,size_t k,float* a,float* b,float* c,float* bias)
+__global__ void cuda_sgemm_forward(size_t m,size_t n,size_t k,float* a,float* b,float* c,float *bias)
 {
+
+    
+
     const int tx = threadIdx.x;
     const int ty = threadIdx.y;
     const int bx = blockIdx.x;
     const int by = blockIdx.y;
-  
-    float *begin_a = a + by * BLOCK * k;
-    float *begin_b = b + bx * BLOCK;
+    //printf("bx=%d by=%d \n",bx,by);
+    int global_idx= bx*BLOCK+tx;
+    int global_idy= by*BLOCK+ty;
+
+    float *begin_a = a + bx * BLOCK * k;
     float *end_a = begin_a + k;
-  
+    float *a_bottom = a+(m-1)*k;
+    float *a_block_bottom = begin_a+(BLOCK-1)*k;
+    int a_x_gap = (a_bottom>=a_block_bottom) ? BLOCK : (BLOCK-(a_block_bottom-a_bottom)/k);
+
+
+    float *begin_b = b + by * BLOCK;
+    float *end_b = b+(k-1)*n;
+    float *b_right = b+n;
+    float *b_block_right = begin_b+BLOCK;
+    int b_y_gap = (b_right>=b_block_right) ? BLOCK : (BLOCK-(b_block_right-b_right));
+        
+
     float sum = 0.f;
+    bool flag=1;
     for (float *a_ptr = begin_a, *b_ptr = begin_b; a_ptr < end_a;
          a_ptr += BLOCK, b_ptr += BLOCK * n) {
-      __shared__ float ashare[BLOCK][BLOCK];
-      __shared__ float bshare[BLOCK][BLOCK];
-  
-      ashare[ty][tx] = a_ptr[ty * k + tx];
-      bshare[ty][tx] = b_ptr[ty * n + tx];
-      __syncthreads();
-  
+
+        __shared__ float ashare[BLOCK][BLOCK];
+        __shared__ float bshare[BLOCK][BLOCK];
+        __shared__ float bias_share[BLOCK];
+    
+        float* a_block_right = a_ptr+BLOCK;
+        int a_y_gap = (end_a>=a_block_right) ? BLOCK : (BLOCK-(a_block_right-end_a));
+
+        float* b_block_bottom = b_ptr+(BLOCK-1) * n;
+        int b_x_gap = (end_b>=b_block_bottom) ? BLOCK : (BLOCK-(b_block_bottom-end_b)/n);
+
+    
+        
+
+        if(tx<a_x_gap&&ty<a_y_gap) ashare[tx][ty] = a_ptr[tx * k + ty];
+        if(tx<b_x_gap&&ty<b_y_gap) bshare[tx][ty] = b_ptr[tx * n + ty];
+        if(tx<a_x_gap) bias_share[tx] = bias[(begin_a-a)/k+tx];
+        __syncthreads();
+
+        // if(num==1&&global_idx==20&&global_idy==5)
+        // {
+        //     for(int i=0;i<BLOCK;i++)
+        //     {
+        //         for(int j=0;j<BLOCK;j++)
+        //         {
+        //             printf("%1.f ",ashare[i][j]);
+        //         }
+        //         printf("\n");
+        //     } 
+        // }
+
+        // if(num==1&&global_idx==20&&global_idy==5) printf("**************\n");
+
+        // //if(num==1&&global_idx==20&&global_idy==5) printf("a_x=%d a_y=%d b_x=%d by=%d\n",a_x_gap,a_y_gap,b_x_gap,b_y_gap);
+
+        // if(num==1&&global_idx==20&&global_idy==5)
+        // {
+        //     for(int i=0;i<BLOCK;i++)
+        //     {
+        //         for(int j=0;j<BLOCK;j++)
+        //         {
+        //             printf("%1.f ",bshare[i][j]);
+        //         }
+        //         printf("\n");
+        //     } 
+        // }  
+
+        // if(num==1&&global_idx==0&&global_idy==0)
+        // {
+        //     for(int i=0;i<BLOCK;i++)
+        //     {
+        //         for(int j=0;j<BLOCK;j++)
+        //         {
+        //             printf("%1.f ",ashare[i][j]);
+        //         }
+        //         printf("\n");
+        //     } 
+        // }
+
+        // if(num==1&&global_idx==16&&global_idy==16) printf("**************\n");
+
+
+
   #pragma unroll
       for (int kk = 0; kk < BLOCK; ++kk) {
-        sum += ashare[ty][kk] * bshare[kk][tx];
+        sum += ashare[tx][kk] * bshare[kk][ty];
+      }
+      if(flag) 
+      {
+          sum+=bias_share[tx];
+          flag=0;
       }
       __syncthreads();
+
+      ashare[tx][ty]=0;
+      bshare[tx][ty]=0; 
+      __syncthreads();
     }
-  
-    c[(BLOCK * by + ty) * n + BLOCK * bx + tx] = sum;
+
+    if(global_idx<m&&global_idy<n)
+    {
+        // printf("global_idx=%d global_idy=%d sum=%f\n",global_idx,global_idy,sum);
+        c[global_idx*n+global_idy] = sum;
+    }
+
 }
-
-
 void cuda_sgemm(size_t m,size_t n,size_t k,float* h_a,float* h_b,float* h_c,float* h_bias)
 {
     float *d_a,*d_b,*d_c,*d_bias;
@@ -113,11 +230,19 @@ void cuda_sgemm(size_t m,size_t n,size_t k,float* h_a,float* h_b,float* h_c,floa
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&milliseconds, begin, stop);
-    printf("m=%ld,n=%ld,k=%ld,  gpu totoal time = %f ms\n",m,k,n,milliseconds);
+    printf("m=%ld,n=%ld,k=%ld,  gpu totoal time = %f ms\n",m,n,k,milliseconds);
 
 
     cudaMemcpy(h_c,d_c,c_nbytes,cudaMemcpyDeviceToHost);
-
+    // for (int i = 0; i < m; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         printf("%1.f ",h_c[i * n + j]);
+    //     }
+    //     printf("\n");
+    // }
+    // /printf("******************gpu result\n");
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
@@ -131,7 +256,7 @@ void test_result()
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 10);
     
-    for (int m = 1, n = 3, k = 1; k < 4096; m = m + 10, n = n * 2, k = k * 2)
+    for (int m = 1, n = 3, k = 2; k <=4096; m = m + 10, n = n * 2, k = k * 2)
     {
         float *h_a, *h_b, *h_c,*h_bias, *result;
         size_t a_nbytes = m * k * sizeof(float);
@@ -156,6 +281,16 @@ void test_result()
             }
         }
 
+        // for (int i = 0; i < m; i++)
+        // {
+        //     for (int j = 0; j < k; j++)
+        //     {
+        //         printf("%1.f ",h_a[i * k + j]);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("******************8\n");
+
         for (int i = 0; i < k; i++)
         {
             for (int j = 0; j < n; j++)
@@ -164,15 +299,35 @@ void test_result()
             }
         }
 
+        // for (int i = 0; i < k; i++)
+        // {
+        //     for (int j = 0; j < n; j++)
+        //     {
+        //         printf("%1.f ",h_b[i * n + j]);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("******************8\n");
+
         for (int i = 0; i < m; i++)
         {
-            h_bias[i] = 0;
+            h_bias[i] =  dis(gen);
+            //h_bias[i] =  0;
         }
 
         cuda_sgemm(m,n,k,h_a,h_b,h_c,h_bias);
         sgemm(m,n,k,h_a,h_b,result,h_bias);
         compare_array(result,h_c,m*n);
 
+        // for (int i = 0; i < m; i++)
+        // {
+        //     for (int j = 0; j < n; j++)
+        //     {
+        //         printf("%1.f ",result[i * n + j]);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("******************cpu result\n");
 
         free(h_a); 
         free(h_b); 
